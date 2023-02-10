bedbac5dbd7dcc7921130e9f3d1451a5fd2b8e18
sasha <fi_fimova@mail.ru>
2023-02-10 18:06:15 +0300

    ex1 is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        int days = 1000;
+        int seconds = days * 24 * 60 * 60;
+        long distance = seconds * 300000L;
+        System.out.println("За 1000 дней свет пройдёт расстояние " + distance + " км");
     }
 }
